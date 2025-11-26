146fb378b8e92942b154397e58681d9fdfc0bbdf
lakhdive-rmc <lakhdive@gmail.com>
2025-11-26 15:09:56 +0530

    Empty Student Class

---
+++ student.py
@@ -0,0 +1,11 @@
+class Students:
+    """
+    An empty class representing a students.
+    The 'pass' keyword is used to fill the empty block.
+    """
+    pass
+
+# Instance of the empty class
+s1 = Students()
+
+print(type(s1))
